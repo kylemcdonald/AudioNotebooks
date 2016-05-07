aa2572db7fea434f5c7069da2303652f11e8eec2
Kyle McDonald <kyle@kylemcdonald.net>
2016-05-07 06:05:25 -0400

    some work with hdf5 and pixelrnn style networks

--- utils/ffmpeg_load_audio.py
+++ utils/ffmpeg_load_audio.py
@@ -10,8 +10,42 @@ import subprocess as sp
 import os
 DEVNULL = open(os.devnull, 'w')
 
+# attempts to handle all float/integer conversions with and without normalizing
+def convert_bit_depth(y, in_type, out_type, normalize=False):
+    in_type = np.dtype(in_type).type
+    out_type = np.dtype(out_type).type
+    
+    if normalize:
+        peak = np.abs(y).max()
+        if peak == 0:
+            normalize = False
+            
+    if issubclass(in_type, np.floating):
+        if normalize:
+            y /= peak
+        if issubclass(out_type, np.integer):
+            y *= np.iinfo(out_type).max
+        y = y.astype(out_type)
+    elif issubclass(in_type, np.integer):
+        if issubclass(out_type, np.floating):
+            y = y.astype(out_type)
+            if normalize:
+                y /= peak
+        elif issubclass(out_type, np.integer):
+            in_max = peak if normalize else np.iinfo(in_type).max
+            out_max = np.iinfo(out_type).max
+            if out_max > in_max:
+                y = y.astype(out_type)
+                y *= (out_max / in_max)
+            elif out_max < in_max:
+                y /= (in_max / out_max)
+                y = y.astype(out_type)
+    return y
+
 # load_audio can not detect the input type
 def ffmpeg_load_audio(filename, sr=44100, mono=False, normalize=True, in_type=np.int16, out_type=np.float32):
+    in_type = np.dtype(in_type).type
+    out_type = np.dtype(out_type).type
     channels = 1 if mono else 2
     format_strings = {
         np.float64: 'f64le',
@@ -41,16 +75,13 @@ def ffmpeg_load_audio(filename, sr=44100, mono=False, normalize=True, in_type=np
                 raw += data
             else:
                 break
-    audio = np.fromstring(raw, dtype=in_type).astype(out_type)
+    audio = np.fromstring(raw, dtype=in_type)
     if channels > 1:
         audio = audio.reshape((-1, channels)).transpose()
+
     if audio.size == 0:
-        return audio, sr
-    if issubclass(out_type, np.floating):
-        if normalize:
-            peak = np.abs(audio).max()
-            if peak > 0:
-                audio /= peak
-        elif issubclass(in_type, np.integer):
-            audio /= np.iinfo(in_type).max
+        return audio.astype(out_type), sr
+    
+    audio = convert_bit_depth(audio, in_type, out_type, normalize)
+
     return audio, sr
